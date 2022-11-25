79ecb20983770b7bff888742e25127154e311e8a
Rohit Shakya <rohit.rkshakya@gmail.com>
2022-11-25 19:52:51 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author : Rohit Shakya
+ * Date   : July-2020
+ * Compiler : g++ 4.9.2
+ * Flags    : -std=c++14
+ * Time complexity : 
+ * Title : 
+ */
+ #include<iostream>
+ using namespace std;
+ int main(){
+ 	cout<<"Hello priya";
+ 	return 0;
+ }
+
